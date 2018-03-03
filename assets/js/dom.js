80db8e87cf26766e22508cc4bbef27e448d971d5
weng <1906338209@qq.com>
2018-03-03 20:51:28 +0800

    index2

--- assets/js/dom.js
+++ assets/js/dom.js
@@ -10,7 +10,35 @@ const dom0=` <div id="dowebok">
 
 
 const dom1=` <div id="dowebok">
-                <div class="section"></div></div>`
+                <div class="section">
+                <h2 class="side-title">摘要</h2>
+                <h2 class="content-title">摘要</h2>
+                <div class="long-words-box" id="word1">
+                      111111111这里是 简介 这里是简介这里是 简介 这里是简介
+                    这里是 简介 这里是简介这里是 简介 这里是简介这里是 简介 这里是简介这里是 简介 这里是简介这里是 简介 这里是简介这里是 简介 这里是简介这里是 简介 这里是简介这里是 简介 这里是简介                </div>
+
+            </div>
+            <div class="section">
+                <h2 class="side-title">功能1 识别</h2>
+                <div class="video-area"></div>
+                <div class="btn-box"> <div class="btn-inner">试一试</div> </div>
+                <!--<div class="slide"><h3>第二屏的第一屏</h3></div>-->
+                <!--<div class="slide"><h3>第二屏的第二屏</h3></div>-->
+                <!--<div class="slide"><h3>第二屏的第三屏</h3></div>-->
+            </div>
+            <div class="section">
+                <h2 class="side-title" style="margin-bottom: 10px;width: 90%">功能2 生成</h2>
+
+                <div class="am-g two-boxes" style="height: 30vh" id="img-box">
+                    <div class="am-u-sm-3" ><img src="assets/img/a.png" alt=""></div>
+                    <div class="am-u-sm-1" ><img src="assets/img/plus.svg" alt=""></div>
+                    <div class="am-u-sm-3" ><img src="assets/img/a.png" alt=""></div>
+                    <div class="am-u-sm-1" ><img src="assets/img/equal.svg" alt=""></div>
+                    <div class="am-u-sm-4"><img src="assets/img/a.png" alt=""></div>
+
+                </div>
+            </div>
+              </div>`
 
 
 
