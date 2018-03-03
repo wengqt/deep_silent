5c0f5ce67ce1e0b587c7b48377823753d59eff82
weng <1906338209@qq.com>
2018-03-03 16:18:22 +0800

    fullpage

--- assets/js/app.js
+++ assets/js/app.js
@@ -19,6 +19,9 @@
         alert('new rating is ' + value)
       }
     })
+
+
+
     window.pageIndex=0;
 
     var bar = document.getElementById('nav-bar').children[0].children;
@@ -92,92 +95,13 @@ function switchPages(lastIndex,nextIndex) {
   $dom.html(dowebok2);
 
   buildFullpage();
-  // $dom.fullpage.reBuild();
-  // $last.animateCss('bounceOutUp', function() {
-  //   // Do somthing after animation
-  //   $last.css('display','none')
-  //
-  //   $next.css('display','block')
-  //   $next.animateCss('bounceInUp');
-  //
-  //
-  // });
+
 
 
 }
 
 
-const dowebok=`<div class="section">
-                <div class="am-g two-boxes" >
-                    <div class="am-u-sm-6 video-box">视频</div>
-                    <div class="am-u-sm-6 video-box">文字（唇语翻译结果）</div>
-                </div>
-                <div class="bottom-options ">
-                    <div class="btn-box"> <div class="btn-inner">选择视频</div> </div>
-                    <div class="btn-box"> <div class="btn-inner">开始测试</div> </div>
-                    <div class="starrr"></div>
-                    <div class="btn-box"> <div class="btn-inner">打分反馈</div> </div>
-                </div>
 
-
-            </div>
-            <div class="section">
-                <div class="am-g two-boxes" style="height: 30vh">
-                    <div class="am-u-sm-6" id="pic1-left">图片</div>
-                    <div class="am-u-sm-6 " id="pic2-right">高光图</div>
-                </div>
-                <div class="long-words-box" id="word1">
-                    111111111
-                </div>
-                <!--<div class="slide"><h3>第二屏的第一屏</h3></div>-->
-                <!--<div class="slide"><h3>第二屏的第二屏</h3></div>-->
-                <!--<div class="slide"><h3>第二屏的第三屏</h3></div>-->
-            </div>
-            <div class="section">
-                <div class="am-g two-boxes" style="height: 30vh">
-                    <div class="am-u-sm-6" id="pic3-left">图片</div>
-                    <div class="am-u-sm-6"><div id="word2">介绍介绍2222222222222</div> </div>
-
-                </div>
-            </div>`
-
-
-const dowebok2=`<div id="dowebok">
-               <div class="section">
-                <div class="am-g two-boxes" >
-                    <div class="am-u-sm-6 video-box">视频22222</div>
-                    <div class="am-u-sm-6 video-box">文字（唇语翻译结果）</div>
-                </div>
-                <div class="bottom-options ">
-                    <div class="btn-box"> <div class="btn-inner">开始摄像</div> </div>
-                    <div class="btn-box"> <div class="btn-inner">开始测试</div> </div>
-                    <div class="starrr"></div>
-                    <div class="btn-box"> <div class="btn-inner">打分反馈</div> </div>
-                </div>
-
-
-            </div>
-            <div class="section">
-                <div class="am-g two-boxes" style="height: 30vh">
-                    <div class="am-u-sm-6" id="pic1-left">图片</div>
-                    <div class="am-u-sm-6 " id="pic2-right">高光图</div>
-                </div>
-                <div class="long-words-box" id="word1">
-                    111111111
-                </div>
-                <!--<div class="slide"><h3>第二屏的第一屏</h3></div>-->
-                <!--<div class="slide"><h3>第二屏的第二屏</h3></div>-->
-                <!--<div class="slide"><h3>第二屏的第三屏</h3></div>-->
-            </div>
-            <div class="section">
-                <div class="am-g two-boxes" style="height: 30vh">
-                    <div class="am-u-sm-6" id="pic3-left">图片</div>
-                    <div class="am-u-sm-6"><div id="word2">介绍介绍2222222222222</div> </div>
-
-                </div>
-            </div>
-            <div class="section"></div>
-            </div>`
 
 
 $(function(){
